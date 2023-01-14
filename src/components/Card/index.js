998197b21ca82c1dc2e5d56eda9ebde15a3ed692
gurh4nn <gurhan2015@gmail.com>
2023-01-14 14:00:58 +0300

    add card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,25 @@
-import Styles from './card.module.scss'
-const Card = () => (
-    <div className={Styles.card}>
-        card
+import Styles from "./card.module.scss";
+const Card = ({ data }) => (
+  <div className={Styles.card}>
+    <div className={Styles.card__content}>
+      <div className={Styles.card__image}>
+        <img className={Styles.card__image__img} src={data?.ImageUrl} alt="" />
+        <div className={Styles.card__image__brand}>
+          <img src={data?.BrandIconUrl} alt="" />
+        </div>
+      </div>
+      <div
+        className={Styles.card__title}
+        dangerouslySetInnerHTML={{ __html: data?.Title }}
+      />
+      <div
+        className={Styles.card__link}
+        style={{ color: data?.PromotionCardColor }}
+      >
+        Daha Daha
+      </div>
     </div>
-)
-
+  </div>
+);
 
 export default Card;
